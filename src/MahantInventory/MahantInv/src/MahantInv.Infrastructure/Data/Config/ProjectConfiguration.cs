1eb0e9d10abf4ce6bf79f40083deb5b7bafa94b5
milanmstc <mpatel@msdgtl.com>
2021-10-16 16:13:24 +0530

    Add default identity tables

--- src/MahantInventory/MahantInv/src/MahantInv.Infrastructure/Data/Config/ProjectConfiguration.cs
+++ src/MahantInventory/MahantInv/src/MahantInv.Infrastructure/Data/Config/ProjectConfiguration.cs
@@ -4,13 +4,13 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MahantInv.Infrastructure.Data.Config
 {
-    public class ProjectConfiguration : IEntityTypeConfiguration<Project>
-    {
-        public void Configure(EntityTypeBuilder<Project> builder)
-        {
-            builder.Property(p => p.Name)
-                .HasMaxLength(100)
-                .IsRequired();
-        }
-    }
+    //public class ProjectConfiguration : IEntityTypeConfiguration<Project>
+    //{
+    //    public void Configure(EntityTypeBuilder<Project> builder)
+    //    {
+    //        builder.Property(p => p.Name)
+    //            .HasMaxLength(100)
+    //            .IsRequired();
+    //    }
+    //}
 }
--- src/MahantInventory/MahantInv/src/MahantInv.Infrastructure/Data/Config/ToDoConfiguration.cs
+++ src/MahantInventory/MahantInv/src/MahantInv.Infrastructure/Data/Config/ToDoConfiguration.cs
@@ -4,12 +4,12 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace MahantInv.Infrastructure.Data.Config
 {
-    public class ToDoConfiguration : IEntityTypeConfiguration<ToDoItem>
-    {
-        public void Configure(EntityTypeBuilder<ToDoItem> builder)
-        {
-            builder.Property(t => t.Title)
-                .IsRequired();
-        }
-    }
+    //public class ToDoConfiguration : IEntityTypeConfiguration<ToDoItem>
+    //{
+    //    public void Configure(EntityTypeBuilder<ToDoItem> builder)
+    //    {
+    //        builder.Property(t => t.Title)
+    //            .IsRequired();
+    //    }
+    //}
 }
