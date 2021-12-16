22a1763a7c0643a63f66c99bf830f73d8a331a36
milanmstc <mpatel@msdgtl.com>
2021-12-16 11:20:03 +0530

    Storage & unittype view

---
+++ src/MahantInventory/MahantInv.Web/Controllers/MetaDataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MahantInv.Web.Controllers
+{
+    public class MetaDataController : Controller
+    {
+        public IActionResult UnitType()
+        {
+            return View();
+        }
+        public IActionResult Storage()
+        {
+            return View();
+        }
+    }
+}
