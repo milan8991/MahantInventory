8106db2627fe1d03018f6ab31b540cf6f5a6b420
milanmstc <mpatel@msdgtl.com>
2022-01-24 10:34:08 +0530

    Add HomeAPi ctrl

---
+++ src/MahantInventory/MahantInv.Web/Api/HomeApiController.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MahantInv.Web.Api
+{
+    public class HomeApiController : BaseApiController
+    {
+        public HomeApiController(IMapper mapper) : base(mapper)
+        {
+        }
+    }
+}
